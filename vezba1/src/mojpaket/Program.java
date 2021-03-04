2a426cde93828eb1025cc4291be747fb8c6b5533
Aleksandar Lukić <93lukic@live.com>
2021-03-04 12:23:08 +0100

    Izmenjen ispis

--- vezba1/src/mojpaket/Program.java
+++ vezba1/src/mojpaket/Program.java
@@ -2,7 +2,7 @@ package mojpaket;
 
 public class Program {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello!");
         System.out.println("Hello World!");
     }
 }
