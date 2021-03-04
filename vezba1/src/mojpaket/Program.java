12e2477b4ea1249d19a64fc75c81d89903b43009
Aleksandar Lukić <93lukic@live.com>
2021-03-04 12:16:31 +0100

    Dodat ispis

--- vezba1/src/mojpaket/Program.java
+++ vezba1/src/mojpaket/Program.java
@@ -3,5 +3,6 @@ package mojpaket;
 public class Program {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
